9bdbf2565915e37f401e2e65b4b5bb91dbc9c79c
nNoidea <58337790+nNoidea@users.noreply.github.com>
2026-09-24 01:22:37 +0200

    fix ci and setup local ci like testing enviroment.

--- src/bar.rs
+++ src/bar.rs
@@ -497,7 +497,6 @@ mod tests {
             .chain(cfg.modules.end_modules().iter())
         {
             assert!(resolve_module_placement(name).is_some(), "unresolvable module '{name}'");
-            assert!(false == true);
         }
     }
 
